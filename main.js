e98a13e3e1b3f5dc999359048fb0fc65a4893578
101ta28 <workta28@gmail.com>
2023-10-27 23:55:12 +0900

    fix typo

--- main.js
+++ main.js
@@ -44,27 +44,29 @@ let instructions = {
   //   "</div>" +
   //   " <p>Press any key to begin.</p> ",
   stimulus: function () {
-    return "<p>In this experiment, a circle will appear in the center " + " of the screen.</p><p>If the circle is <strong>blue</strong>, " + " press the letter F on the keyboard as fast as you can.</p> " + " <p>If the circle is <strong>orange</strong>, press the letter J " + " as fast as you can.</p> " + " <div style='width: 700px;'>" + " <div style='float: left;'><img src=" + repo_site + "img/blue.png></img>" + " <p class='small'><strong>Press the F key</strong></p></div>" + " <div style='float: right;'><img src=" + repo_site + "img/orange.png></img> " + " <p class='small'><strong>Press the J key</strong></p></div>" + "</div>" + " <p>Press any key to begin.</p> ";
+    return "<div style='float: left;'><img src=" + repo_site + "/img/blue.png></img>" +
+      "<p class='small'><strong>Press the F key</strong></p></div>" +
+      "<div style='float: right;'><img src=" + repo_site + "/img/orange.png></img> " +
+      "<p class='small'><strong>Press the J key</strong></p></div>";
   },
   post_trial_gap: 2000
 };
 timeline.push(instructions);
 
 /* test trials */
-let test_stimuli = [
+let test_stimulus = [
   // { stimulus: repo_site + "img/blue.png", correct_response: 'f' },
   {
     stimulus: function () {
-      return repo_site + "img/blue.png";
-    }
-    , correct_response: 'f'
+      return repo_site + "/img/blue.png";
+    },
+    correct_response: 'f'
   },
-  // { stimulus: repo_site + "img/orange.png", correct_response: 'j' }
   {
     stimulus: function () {
-      return repo_site + "img/orange.png";
-    }
-    , correct_response: 'j'
+      return repo_site + "/img/orange.png";
+    },
+    correct_response: 'j'
   }
 ];
 
@@ -104,7 +106,7 @@ let test = {
 
 let test_procedure = {
   timeline: [fixation, test],
-  timeline_letiables: test_stimuli,
+  timeline_letiables: test_stimulus,
   repetitions: 5,
   randomize_order: true
 }
