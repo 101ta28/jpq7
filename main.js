dd9f636ba82facb252228e19fbf74bfd6df91201
101ta28 <workta28@gmail.com>
2023-10-28 00:47:54 +0900

    fix typo etc...

--- main.js
+++ main.js
@@ -3,17 +3,11 @@ let repo_site = "https://imaru.github.io/jqTemplate/";
 /* create timeline */
 let timeline = [];
 
-console.log('main.js');
-console.log(jsPsychHtmlKeyboardResponse);
-
 // /* preload images */
 let preload = {
   // type: 'preload',
   type: jsPsychPreload,
   images: [repo_site + 'img/blue.png', repo_site + 'img/orange.png'],
-  on_finish: function () {
-    console.log('preload finished');
-  }
 }
 timeline.push(preload);
 
@@ -24,40 +18,36 @@ let welcome = {
   // stimulus: function () {
   //   return "<p>Welcome to the experiment. Press any key to begin.</p>";
   // },
-  on_finish: function () {
-    console.log('preload finished');
-  }
 };
-
 timeline.push(welcome);
 
 /* define instructions trial */
 let instructions = {
   type: jsPsychHtmlKeyboardResponse,
-  // stimulus: "<p>In this experiment, a circle will appear in the center " +
-  //   " of the screen.</p><p>If the circle is <strong>blue</strong>, " +
-  //   " press the letter F on the keyboard as fast as you can.</p> " +
-  //   " <p>If the circle is <strong>orange</strong>, press the letter J " +
-  //   " as fast as you can.</p> " +
-  //   " <div style='width: 700px;'>" +
-  //   " <div style='float: left;'><img src=" + repo_site + "'img/blue.png'></img>" +
-  //   " <p class='small'><strong>Press the F key</strong></p></div>" +
-  //   " <div style='float: right;'><img src=" + repo_site + "'img/orange.png'></img> " +
-  //   " <p class='small'><strong>Press the J key</strong></p></div>" +
-  //   "</div>" +
-  //   " <p>Press any key to begin.</p> ",
-  stimulus: function () {
-    return "<div style='float: left;'><img src=" + repo_site + "/img/blue.png></img>" +
-      "<p class='small'><strong>Press the F key</strong></p></div>" +
-      "<div style='float: right;'><img src=" + repo_site + "/img/orange.png></img> " +
-      "<p class='small'><strong>Press the J key</strong></p></div>";
-  },
+  stimulus: "<p>In this experiment, a circle will appear in the center " +
+    " of the screen.</p><p>If the circle is <strong>blue</strong>, " +
+    " press the letter F on the keyboard as fast as you can.</p> " +
+    " <p>If the circle is <strong>orange</strong>, press the letter J " +
+    " as fast as you can.</p> " +
+    " <div style='width: 700px;'>" +
+    " <div style='float: left;'><img src=" + repo_site + "img/blue.png></img>" +
+    " <p class='small'><strong>Press the F key</strong></p></div>" +
+    " <div style='float: right;'><img src=" + repo_site + "img/orange.png></img> " +
+    " <p class='small'><strong>Press the J key</strong></p></div>" +
+    "</div>" +
+    " <p>Press any key to begin.</p> ",
+  // stimulus: function () {
+  //   return "<div style='float: left;'><img src=" + repo_site + "/img/blue.png></img>" +
+  //     "<p class='small'><strong>Press the F key</strong></p></div>" +
+  //     "<div style='float: right;'><img src=" + repo_site + "/img/orange.png></img> " +
+  //     "<p class='small'><strong>Press the J key</strong></p></div>";
+  // },
   post_trial_gap: 2000
 };
 timeline.push(instructions);
 
 /* test trials */
-let test_stimulus = [
+let test_stimuli = [
   // { stimulus: repo_site + "img/blue.png", correct_response: 'f' },
   {
     stimulus: function () {
@@ -75,10 +65,10 @@ let test_stimulus = [
 
 let fixation = {
   type: jsPsychHtmlKeyboardResponse,
-  // stimulus: '<div style="font-size:60px;">+</div>',
-  stimulus: function () {
-    return '<div style="font-size:60px;">+</div>';
-  },
+  stimulus: '<div style="font-size:60px;">+</div>',
+  // stimulus: function () {
+  //   return '<div style="font-size:60px;">+</div>';
+  // },
   choices: "NO_KEYS",
   trial_duration: function () {
     return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
@@ -90,17 +80,17 @@ let fixation = {
 
 let test = {
   type: jsPsychImageKeyboardResponse,
-  // stimulus: jsPsych.timelineletiable('stimulus', true),
-  stimulus: function () {
-    return jsPsych.timelineletiable('stimulus');
-  },
+  stimulus: jsPsych.timelineVariable('stimulus'),
+  // stimulus: function () {
+  //   return jsPsych.timelineVariable('stimulus');
+  // },
   choices: ['f', 'j'],
   data: {
     task: 'response',
-    // correct_response: jsPsych.timelineletiable('correct_response')
-    correct_response: function () {
-      return jsPsych.timelineletiable('correct_response');
-    }
+    correct_response: jsPsych.timelineVariable('correct_response')
+    // correct_response: function () {
+    //   return jsPsych.timelineVariable('correct_response');
+    // }
   },
   on_finish: function (data) {
     data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
@@ -109,14 +99,13 @@ let test = {
 
 let test_procedure = {
   timeline: [fixation, test],
-  timeline_letiables: test_stimulus,
+  timeline_variable: test_stimuli,
   repetitions: 5,
   randomize_order: true
 }
 timeline.push(test_procedure);
 
 /* define debrief */
-
 let debrief_block = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: function () {
@@ -130,5 +119,4 @@ let debrief_block = {
             <p>Press any key to complete the experiment. Thank you!</p>`;
   }
 };
-
 timeline.push(debrief_block);
