c586fdf05a6f98dd3f73a826947de3c7e8c45aa2
101ta28 <workta28@gmail.com>
2023-10-28 12:12:43 +0900

    welcomeを非表示

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let welcome = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: "Welcome to the experiment. Press any key to begin.",
 };
-timeline.push(welcome);
+// timeline.push(welcome);
 
 let instructions = {
   type: jsPsychHtmlKeyboardResponse,
