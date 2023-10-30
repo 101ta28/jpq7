d985405c8cabd12782005f5058d212acf98562e1
101ta28 <workta28@gmail.com>
2023-10-30 14:38:18 +0900

    バージョン6用のものを作成

---
+++ exp1.js
@@ -0,0 +1,19 @@
+const repo_site = "https://101ta28.github.io/jpq7/";
+
+/* create timeline */
+let timeline = [];
+
+const hello_trial = {
+    type: 'html-keyboard-response',
+    stimulus: function () {
+        const html =
+            '<img id="rotate-image" src="${repo_site}jspsych-6.3.1/examples/img/9.gif" style="transition: transform 2s;">';
+        setTimeout(() => {
+            document.getElementById("rotate-image").style.transform =
+                "rotate(360deg)";
+        }, 0);
+        return html;
+    },
+};
+
+timeline.push(hello_trial);
---
+++ exp2.js
@@ -0,0 +1,45 @@
+const repo_site = "https://101ta28.github.io/jpq7/";
+
+/* create timeline */
+let timeline = [];
+
+const hello_trial = {
+    type: 'html-keyboard-response',
+    stimulus: function () {
+        const html = '<img id="rotate-image" src="${repo_site}jspsych-6.3.1/examples/img/9.gif">';
+        return html;
+    },
+    on_load: function () {
+        let image = document.getElementById("rotate-image");
+        let startAngle = 0;
+        let rotation = 0;
+
+        image.onmousedown = function (event) {
+            // Get the center of the image
+            let rect = image.getBoundingClientRect();
+            let centerX = rect.left + rect.width / 2;
+            let centerY = rect.top + rect.height / 2;
+
+            startAngle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
+
+            document.onmousemove = function (event) {
+                let moveAngle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI);
+                let angleDiff = moveAngle - startAngle;
+
+                rotation += angleDiff;
+
+                image.style.transition = "none"; // Disable smooth transition while dragging
+                image.style.transform = `rotate(${rotation}deg)`;
+
+                startAngle = moveAngle; // Set the new start angle for the next movement
+            };
+
+            document.onmouseup = function () {
+                document.onmousemove = null;
+                document.onmouseup = null;
+            };
+        };
+    },
+};
+
+timeline.push(hello_trial);
